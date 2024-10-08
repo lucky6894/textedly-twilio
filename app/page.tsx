62585dce439e36f9e71647f6909302e01ce2eea5
lucky6894 <full.luck@outlook.com>
2024-10-08 19:56:54 -0400

    dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
 "use client"
 
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_API;
+
 export default function HomePage() {
+  const [statistics, setStatistics] = useState<any>();
+
+  useEffect(() => {
+    axios.get("/statistics").then(({data}) => {
+      setStatistics(data);
+    });
+  }, []);
+
   return (
-    <div></div>
+    <div>
+      <div className="border-2 w-64 rounded-2xl p-4 bg-gray-200">
+        <p className="font-semibold mb-2">Balance</p>
+        <p className="font-bold text-lg">${statistics?.balance.amount}</p>
+      </div>
+    </div>
   );
 }
