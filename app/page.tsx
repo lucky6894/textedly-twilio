4bcdc9a6dbaeeb8daf46670361acd0e599b832f5
lucky6894 <full.luck@outlook.com>
2024-10-10 03:21:45 -0400

    implement emoji

--- app/page.tsx
+++ app/page.tsx
@@ -7,35 +7,31 @@ import {
   DropdownMenuContent,
   DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
-import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
-} from "@/components/ui/hover-card";
+import { Input } from "@/components/ui/input";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import {
   Select,
   SelectContent,
-  SelectGroup,
   SelectItem,
   SelectTrigger,
   SelectValue
 } from "@/components/ui/select";
 import { cn } from "@/lib/utils";
 import { DropdownMenuItem } from "@radix-ui/react-dropdown-menu";
+import axios from "axios";
+import EmojiPicker from 'emoji-picker-react';
 import { ChevronDown, ImageIcon, ImagePlayIcon, LaughIcon, Paperclip } from "lucide-react";
 import { useEffect, useState } from "react";
-import axios from "axios";
-import { Input } from "@/components/ui/input";
 
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_API;
 
 export default function HomePage() {
   const [time, setTime] = useState(true);
-  const [calendarOpen, setCalendarOpen] = useState(false);
   const [phoneNumbers, setPhoneNumbers] = useState<string[]>([]);
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
-    axios.get("/phone-numbers").then(({data}) => {
+    axios.get("/phone-numbers").then(({ data }) => {
       setPhoneNumbers(data);
     });
   }, []);
@@ -66,7 +62,10 @@ export default function HomePage() {
         <p className="text-sm text-gray-500 mb-2">Your Message</p>
         <div className="rounded-md w-full border-2 p-0">
           <div></div>
-          <textarea className="border-2 w-full min-h-16 px-3 py-1"></textarea>
+          <textarea className="border-none focus:outline-none w-full px-3 py-1 -mb-2"
+            value={message}
+            onChange={(event) => setMessage(event.target.value)}
+          ></textarea>
         </div>
         <div className="flex justify-between items-center">
           <p className="text-gray-500 text-sm">
@@ -88,10 +87,22 @@ export default function HomePage() {
         </div>
       </div>
       <div className="mt-5">
-        <Button className="h-4 text-blue-500 px-2" variant="link">
-          <LaughIcon size={16} className="mr-1" />
-          Emojis
-        </Button>
+        {/* Emoji */}
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button className="h-4 text-blue-500 px-2" variant="link">
+              <LaughIcon size={16} className="mr-1" />
+              Emojis
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="p-0 border-none">
+            <EmojiPicker
+              onEmojiClick={(emoji) => {
+                setMessage(message => message + emoji.emoji);
+              }}
+            />
+          </PopoverContent>
+        </Popover>
         <Button className="h-4 text-blue-500 px-2" variant="link">
           <ImageIcon size={16} className="mr-1" />
           Photo
@@ -130,22 +141,17 @@ export default function HomePage() {
         <div className="flex gap-5 w-full">
           <div className="w-1/2">
             <p className="text-gray-500">Choose date and time</p>
-            <HoverCard open={calendarOpen}>
-              <HoverCardTrigger>
-                <input
-                  className="border-2 rounded-md h-10 w-full pl-3"
-                  type="text"
-                  onClick={() => setCalendarOpen(true)}
-                  onBlur={() => setCalendarOpen(false)}
-                />
-              </HoverCardTrigger>
-              <HoverCardContent className="w-full p-1">
+            <Popover>
+              <PopoverTrigger className="w-full">
+                <input className="border-2 rounded-md h-10 w-full pl-3" type="text" />
+              </PopoverTrigger>
+              <PopoverContent className="w-full p-1">
                 <Calendar
                   mode="single"
                   className="rounded-md w-full"
                 />
-              </HoverCardContent>
-            </HoverCard>
+              </PopoverContent>
+            </Popover>
           </div>
           <div className="w-1/2">
             <p className="text-gray-500">Time</p>
